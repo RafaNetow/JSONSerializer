d8f1ec710d0f0b1b6366c70e4cfe4737ea416f47
rafanetow <rafanetow@gmail.com>
2015-08-22 21:22:58 -0600

    First Test with default example

---
+++ JsonSerializerProyect/JsonSerializer/Calculetor.cs
@@ -0,0 +1,14 @@
+﻿
+namespace JsonSerializer
+{
+    public class Calculetor
+    {
+        public int FirstNumber { set; private get; }
+        public int SecondNumber { set; private get; }
+
+        public int add()
+        {
+            return SecondNumber + FirstNumber;
+        }
+    }
+}
---
+++ JsonSerializerProyect/JsonSerializerProyect/CalculetorSteps.cs
@@ -0,0 +1,39 @@
+﻿using JsonSerializer;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TechTalk.SpecFlow;
+
+namespace JsonSerializerProyect
+{
+    [Binding]
+    public class CalculetorSteps
+    {
+        private int resultado { get; set; }
+        Calculetor calculadora = new Calculetor();
+       
+
+        [Given(@"I have entered (.*) into the calculator")]
+        public void GivenIHaveEnteredIntoTheCalculator(int p1)
+        {
+            calculadora.FirstNumber = p1;
+        }
+
+        [Given(@"I have also entered (.*) into the calculator")]
+        public void GivenIHaveAlsoEnteredIntoTheCalculator(int p0)
+        {
+            calculadora.SecondNumber = p0;
+        }
+
+        [When(@"I press add")]
+        public void WhenIPressAdd()
+        {
+
+            resultado = calculadora.add();
+        }
+        
+        [Then(@"the result should be (.*) on the screen")]
+        public void ThenTheResultShouldBeOnTheScreen(int expectedResult)
+        {
+            Assert.AreEqual(expectedResult, resultado);
+        }
+    }
+}
